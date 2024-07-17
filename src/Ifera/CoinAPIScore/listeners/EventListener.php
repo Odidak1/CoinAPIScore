3ea877f0e526f2a5356d0b61e2874f14b16a7004
Radit Maulana <raditm100308@gmail.com>
2024-07-17 20:05:03 +0700

    Create EventListener.php

---
+++ src/Ifera/CoinAPIScore/listeners/EventListener.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+namespace Ifera\CoinAPIScore\listeners;
+
+use Ifera\CoinAPIScore\Main;
+use Ifera\ScoreHud\event\PlayerTagUpdateEvent;
+use Ifera\ScoreHud\scoreboard\ScoreTag;
+use onebone\coinapi\event\coin\CoinChangedEvent;
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+use function is_null;
+
+class EventListener implements Listener{
+
+	/** @var Main */
+	private $plugin;
+
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+
+	public function onCoinChange(CoinChangedEvent $event){
+		$username = $event->getUsername();
+
+		if(is_null($username)){
+			return;
+		}
+
+		$player = $this->plugin->getServer()->getPlayerByPrefix($username);
+
+		if($player instanceof Player && $player->isOnline()){
+			(new PlayerTagUpdateEvent($player, new ScoreTag("saungmcscore.coin", (string) $event->getCoin())))->call();
+		}
+	}
+}
